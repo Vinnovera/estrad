c54f8968acc4fe20be24af42991a93f4b6746f4a
Per Stenström <per.stenstrom@gmail.com>
2014-02-13 21:34:50 +0100

    Initial commit

---
+++ Gulpfile.js
@@ -0,0 +1,122 @@
+(function(){
+	"use strict";
+
+var 
+	gulp = require('gulp'),
+	jshint = require('gulp-jshint'),
+	jshintStylish = require('jshint-stylish'),
+	concat = require('gulp-concat'),
+	chokidar = require('chokidar'),
+	chalk = require('chalk'),
+	path = require("path"),
+	spawn = require('child_process').spawn,
+	fs = require('fs'),
+	jshintRc = JSON.parse(fs.readFileSync('./.jshintrc', 'utf-8')),
+	pkg = JSON.parse(fs.readFileSync('./package.json')),
+	paths = {
+		'server': ['./index.js', './lib/**/*.js'],
+		'style': ['./css/**/*.css', './modules/**/*.css', '!css/main.css'],
+		'script': ['./js/**/*.js', './modules/**/*.js', '!js/main.js']
+	},
+	node, jstimeout, csstimeout;
+
+// https://gist.github.com/webdesserts/5632955
+gulp.task('server', function(){
+	if(node) node.kill();
+	node = spawn('node', ['./index.js'], {stdio: 'inherit'});
+
+	node.on('close', function(code) {
+		if(code === 8) {
+			console.log('Error detected, waiting for changes...');
+		}
+	});
+});
+
+gulp.task('watch', function(){
+	/* This watcher will work even when entire directorys are copy-pasted */
+	var watcher = chokidar.watch(['js','modules', 'css'], {ignored: /\/main.|.DS_Store|\/html|\/vendor/, persistent: true, ignoreInitial: true});
+
+	watcher.on('all', function (event, pathname){
+		var ext = path.extname(pathname);
+
+		console.log("[" + chalk.green(pkg.name) + "] File event " + chalk.cyan(event) + ": " + chalk.magenta(pathname));
+
+		switch(ext) {
+			case '':
+				/* dir added or removed */
+				csstask(event, pathname);
+				jstask(event, pathname);
+			break;
+			case '.css':
+				/* css file change */
+				csstask(event, pathname);
+			break;
+			case '.js':
+				/* js file change */
+				jstask(event,pathname);
+			break;
+		}
+	});
+});
+
+gulp.task('default', ['server', 'watch'], function(){
+	// Start the server, if a change is detected restart it
+	gulp.watch(paths.server, ['server']);
+});
+
+function jstask(event, path) {
+	switch(event) {
+		case 'add':
+			clearTimeout(jstimeout);
+			jstimeout = setTimeout(function(){
+				lint(paths.script);
+				jsconcat();
+			},10);
+		break;
+		case 'change':
+			lint(path);
+		case 'unlink':
+			jsconcat();
+		break;
+	}
+}
+
+function lint(path) {
+	return gulp.src(path)
+		.pipe(jshint(jshintRc))
+		.pipe(jshint.reporter(jshintStylish));
+}
+
+function jsconcat() {
+	return gulp.src(paths.script)
+		.pipe(concat('main.js'))
+		.pipe(gulp.dest('./js/'));
+}
+
+function csstask(event, path) {
+	switch(event) {
+		case 'add':
+			clearTimeout(csstimeout);
+			csstimeout = setTimeout(function(){
+				cssconcat();
+			},10);
+		break;
+		case 'change':
+		case 'unlink':
+			cssconcat();
+		break;
+	}
+}
+
+function cssconcat() {
+	return gulp.src(paths.style)
+		.pipe(concat('main.css'))
+		.pipe(gulp.dest('./css/'));
+}
+
+// clean up if an error goes unhandled.
+process.on('exit', function() {
+	if (node) node.kill();
+	watcher.close();
+});
+})();
---
+++ index.js
@@ -0,0 +1,70 @@
+(function(){
+	"use strict";
+	var
+		app = require("http").createServer(handler),
+		path = require("path"),
+		fs = require("fs"),
+		chalk = require("chalk"),
+		autoload = require("./lib/autoload"),
+		template = require("./lib/template"),
+		port = 8080;
+
+		app.listen(port);
+		console.log("[" + chalk.green("server") + "] Server started at: " + chalk.magenta("http://localhost:" + port));
+
+		function handler(req, res) {
+			var 
+				url = req.url,
+				ext;
+
+			if(url === "/") url = "/index.html";
+			ext = path.extname(url);
+
+			switch(ext) {
+				case ".html":
+					console.log("[" + chalk.green("server") + "] Request: " + chalk.magenta(url));
+					getPage(url, function(err, content){
+						if(err) {
+							res.writeHead(500, "server error");
+							res.end();
+							console.log("[" + chalk.red("server") + "] " + err);
+							return;
+						}
+
+						res.writeHead(200);
+						res.end(content);
+					});
+				break;
+				// Static resourses
+				default:
+					fs.readFile(__dirname + url, function(err, data){
+						if (err) {
+							res.writeHead(404, "not found");
+							res.end();
+							return;
+						}
+
+						res.writeHead(200);
+						res.end(data);
+					});
+				break;
+			}
+		}
+
+		function getPage(page, callback){
+			autoload.loadFile(page, function(err, content, obj, root, dependencies, dependees){
+
+				if(err) return callback(err);
+
+				// Solve dependencies
+				template.solveDependencies(obj, dependees, dependencies, function(err, obj){
+					var page;
+					if(err) return callback(err);
+
+					page = template.interpolate(content, obj);
+
+					callback(null, page);
+				});
+			});
+		}
+})();
