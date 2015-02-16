be3459bb7e111795c6d362e9ee469cc155b2e355
Per Stenström <per@vinnovera.se>
2015-02-16 15:55:45 +0100

    Bootstrap require.config.paths on server

--- index.js
+++ index.js
@@ -36,6 +36,39 @@
 	app.get('*.html', handler);
 
 	/**
+	 * Bootstrap require.config.paths for JS files in modules/
+	 */
+	app.get('/' + options.js.paths.src, function(req, res) {
+		fs.exists(process.cwd() + url.parse(req.url).pathname, function(exists) {
+			if(!exists) return headerNotFound(res);
+
+			fs.exists(process.cwd() + '/js/modulesPaths.js', function(exists) {
+				if(!exists) {
+					readContent(req.url, function(err, data) {
+						if(err) headerNotFound(res);
+
+						res.writeHead(200);
+						res.end(data);
+					});
+
+					return;
+				}
+
+				readContent(req.url, function(err, data) {
+					if(err) return headerNotFound(res);
+
+					readContent('/js/modulesPaths.js', function(err, paths) {
+						if(err) return headerNotFound(res);
+
+						res.writeHead(200);
+						res.end(paths + data);
+					});
+				});
+			});
+		}); 
+	});
+
+	/**
 	 * Handle requests for static files
 	 */
 	app.use(express.static(__dirname + '/'));
@@ -76,5 +109,19 @@
 				res.end(content);
 			});
 		});
+	}
+
+	function readContent(filename, callback) {
+		fs.readFile(process.cwd() + filename, {'encoding': 'utf8'}, function(err, data) {
+			if(err) return callback(err);
+
+			callback(null, data);
+		});
+	}
+
+	function headerNotFound(res) {
+		res.writeHead(404, 'Not Found');
+		res.end('Not found');
+		throw err;
 	}
 })();
