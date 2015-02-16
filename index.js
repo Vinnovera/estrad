3a15cf79673e5c0c55d8cf6779900fae0cfcb3a5
Per Stenström <per@vinnovera.se>
2015-02-16 16:05:24 +0100

    refractor readContent

--- index.js
+++ index.js
@@ -6,6 +6,7 @@
 		express = require("express"),
 		app = express(),
 		proxy = require("./lib/proxy"),
+		helper = require('./lib/helper'),
 		partials = require("estrad-template"),
 		extend = require("extend"),
 		fs = require("fs"),
@@ -39,33 +40,24 @@
 	 * Bootstrap require.config.paths for JS files in modules/
 	 */
 	app.get('/' + options.js.paths.src, function(req, res) {
-		fs.exists(process.cwd() + url.parse(req.url).pathname, function(exists) {
-			if(!exists) return headerNotFound(res);
+		helper.readContentIfExists(url.parse(req.url).pathname, function(err, data) {
+			if(err) {
+				res.writeHead(404, 'Not Found');
+				res.end('Not found');
+				throw err;
+			}
 
-			fs.exists(process.cwd() + '/js/modulesPaths.js', function(exists) {
-				if(!exists) {
-					readContent(req.url, function(err, data) {
-						if(err) headerNotFound(res);
-
-						res.writeHead(200);
-						res.end(data);
-					});
-
+			helper.readContentIfExists('/js/modulesPaths.js', function(err, paths) {
+				if(err) {
+					res.writeHead(200);
+					res.end(data);
 					return;
 				}
 
-				readContent(req.url, function(err, data) {
-					if(err) return headerNotFound(res);
-
-					readContent('/js/modulesPaths.js', function(err, paths) {
-						if(err) return headerNotFound(res);
-
-						res.writeHead(200);
-						res.end(paths + data);
-					});
-				});
+				res.writeHead(200);
+				res.end(paths + data);
 			});
-		}); 
+		});
 	});
 
 	/**
@@ -109,19 +101,5 @@
 				res.end(content);
 			});
 		});
-	}
-
-	function readContent(filename, callback) {
-		fs.readFile(process.cwd() + filename, {'encoding': 'utf8'}, function(err, data) {
-			if(err) return callback(err);
-
-			callback(null, data);
-		});
-	}
-
-	function headerNotFound(res) {
-		res.writeHead(404, 'Not Found');
-		res.end('Not found');
-		throw err;
 	}
 })();
