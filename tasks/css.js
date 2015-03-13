8fb112166e9af93aad5d9c9736f33d031df074bf
Per Stenström <per@vinnovera.se>
2015-03-13 16:41:00 +0100

    add minify plugin

--- tasks/css.js
+++ tasks/css.js
@@ -3,8 +3,7 @@ module.exports = function (gulp, options) {
 
 	var
 		concat    = require('gulp-concat'),
-		gstylus   = require('gulp-stylus'),
-		stylus    = require('stylus'),
+		stylus    = require('gulp-stylus'),
 		nib       = require('nib'),
 		extend    = require('extend'),
 		gulpif    = require('gulp-if'),
@@ -12,7 +11,7 @@ module.exports = function (gulp, options) {
 		rename    = require('gulp-rename'),
 		inlineSVG = require('stylus-inline-svg'),
 		path      = require('path'),
-		through2  = require('through2'),
+		minify    = require('gulp-minify-css'),
 
 		// child_process.spawn() that works with windows
 		spawn  = require('win-spawn'),
@@ -87,7 +86,7 @@ module.exports = function (gulp, options) {
 	function stylTask(buildTask) {
 
 		return gulp.src(paths.src)
-			.pipe(gstylus(stylO)
+			.pipe(stylus(stylO)
 				.on('error', stylError)
 			)
 			.pipe(gulp.dest(paths.dest.dir))
@@ -99,33 +98,7 @@ module.exports = function (gulp, options) {
 			 */
 			.pipe(gulpif(!buildTask || !options.uglify, ignore.exclude(true)))
 			
-			/**
-			 * There was an issue running gulp-stylus twice on the same stream. The second stylus compilation didn't seem to do anything.
-			 *
-			 * So we're using this very crude stylus gulp-pluggin to compress the CSS 
-			 */
-			.pipe(through2.obj(function(file, enc, next) {
-				var
-
-					// Turn buffer into string
-					str = file.contents.toString();
-
-				stylus(str)
-					.set('compress', true)
-					.render(function(err, css) {
-
-						// Do not allow silent fails
-						if(err) throw err;
-
-						str = css;
-					});
-
-				// Turn string back into buffer
-				file.contents = new Buffer(str);
-				this.push(file);
-				this.end();
-				next();
-			}))
+			.pipe(minify())
 			.pipe(rename(function(path) {
 				path.extname = '.min' + path.extname;
 			}))
