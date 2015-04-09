77f236bbcbf93c426223263f4b90e8a89b57a0ed
Per Stenström <per@vinnovera.se>
2015-04-09 11:03:05 +0200

    test readContentIfExists

--- test/test-helper.js
+++ test/test-helper.js
@@ -13,6 +13,22 @@ describe('lib/helper.js', function() {
 			});
 		});
 	});
+
+	describe('readContentIfExists', function() {
+		it('should return error if file is missing', function(done) {
+			helper.readContentIfExists('/test/helper/missingFile.txt', function(err) {
+				assert.equal(err instanceof Error, true);
+				done();
+			});
+		});
+
+		it('should read file content', function(done) {
+			helper.readContentIfExists('/test/helper/readContentIfExists.txt', function(err, data) {
+				assert.equal(data, 'foo');
+				done();
+			});
+		});
+	});
 });
 
 })();
